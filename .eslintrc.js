2c86a3e2b2d09e39ccf635070286431f822abf24
Marcelo <marcelodpaternostro@gmail.com>
2022-06-10 20:54:55 -0300

    add eslint + prettier + lint project

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+	root: true,
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				tsconfigRootDir: __dirname,
+				project: ["./tsconfig.json"],
+			},
+			plugins: ["@typescript-eslint"],
+			extends: [
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+			],
+		},
+	],
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+		"prettier",
+	],
+	env: {
+		browser: true,
+		node: true,
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};
