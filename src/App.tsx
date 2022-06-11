51be841895f5e8bd21d4236dd05f7263f51a6415
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-06-11 03:22:13 -0300

    Clean up default app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import fetch from "cross-fetch";
-import logo from "./logo.svg";
 import "./App.css";
 import { NasaApiResponseData } from "./interfaces";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 function App() {
-	const [count, setCount] = useState(0);
 	const [wildfires, setWildfires] = useState<NasaApiResponseData["events"]>([]);
 
 	useEffect(() => {
@@ -27,45 +25,6 @@ function App() {
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Hello Vite + React!</h1>
-				<p>
-					<button type="button" onClick={() => setCount((count) => count + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.tsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					{" | "}
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
-			<main>
-				<h2>All wildfires:</h2>
-				<ul>
-					{wildfires.map((wildfire) => (
-						<li key={wildfire.id}>{wildfire.title}</li>
-					))}
-				</ul>
-			</main>
 			<MapContainer
 				center={[0, 0]}
 				zoom={2}
