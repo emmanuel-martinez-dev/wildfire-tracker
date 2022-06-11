320a4ee6c7d33e6d3b905683058568b370453b73
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-06-11 04:47:05 -0300

    Clear code from Markers change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import fetch from "cross-fetch";
 import "./App.css";
 import { NasaApiResponseData } from "./interfaces";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { MapContainer, TileLayer } from "react-leaflet";
 import Markers from "./Markers";
 
@@ -27,31 +26,15 @@ function App() {
 
 	return (
 		<div className="App">
-			<MapContainer
-				center={[0, 0]}
-				zoom={2}
-				scrollWheelZoom={false}
-				className="h-screen"
-			>
 			<MapContainer center={[0, 0]} zoom={2} className="h-screen">
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
 				{wildfires.map((wildfire) => (
-					<Marker
 					<Markers
 						onClick={openModal}
 						key={wildfire.id}
-						position={
-							wildfire?.geometry[0].coordinates
-								? [
-										wildfire.geometry[0].coordinates[1],
-										wildfire.geometry[0].coordinates[0],
-								  ]
-								: [0, 0]
-						}
-					></Marker>
 						position={wildfire?.geometry[0].coordinates || [0, 0]}
 					></Markers>
 				))}
